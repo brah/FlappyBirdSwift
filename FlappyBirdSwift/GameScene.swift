c11b4eb4fe70c6727f3a2844b2a6e72d401f4013
qbabin <q.babin@gmail.com>
2014-08-11 01:17:34 -0700

    Changed up/downbuilding images to spaceship and asteriods

--- FlappyBirdSwift/GameScene.swift
+++ FlappyBirdSwift/GameScene.swift
@@ -7,6 +7,7 @@
 //
 
 import SpriteKit
+import AVFoundation
 
 class GameScene: SKScene {
     
@@ -15,21 +16,27 @@ class GameScene: SKScene {
     var pipeUpTexture = SKTexture()
     var pipeDownTexture = SKTexture()
     var PipesMoveAndRemove = SKAction()
-    
+    var backgroundMusicPlayer: AVAudioPlayer!
     let pipeGap = 150.0
     
     override func didMoveToView(view: SKView) {
-  
+        var error: NSError?
 
-        /* Setup your scene here */
-//        let myLabel = SKLabelNode()
-//        myLabel.text = "Hello, World!";
-//        myLabel.fontSize = 100;
-//        myLabel.position = CGPoint(x:CGRectGetMaxX(self.frame), y:CGRectGetMaxY(self.frame));
-//        
-//        self.addChild(myLabel)
+        let backgroundMusicURL = NSBundle.mainBundle().URLForResource("background-music", withExtension: "aiff")
+        backgroundMusicPlayer = AVAudioPlayer(contentsOfURL: backgroundMusicURL, error: &error)
+        backgroundMusicPlayer.numberOfLoops = -1
+        backgroundMusicPlayer.play()
+
         
-
+        /* Setup your scene here */
+        //        let myLabel = SKLabelNode()
+        //        myLabel.text = "Hello, World!";
+        //        myLabel.fontSize = 100;
+        //        myLabel.position = CGPoint(x:CGRectGetMaxX(self.frame), y:CGRectGetMaxY(self.frame));
+        //
+        //        self.addChild(myLabel)
+        
+        
         //Physics
         self.physicsWorld.gravity = CGVectorMake(0.0, -5.0);
         
@@ -39,20 +46,21 @@ class GameScene: SKScene {
         
         var BirdTexture = SKTexture(imageNamed:"Spaceship")
         BirdTexture.filteringMode = SKTextureFilteringMode.Nearest
-     
         
+        
+
         
         bird = SKSpriteNode(texture: BirdTexture)
-
+        
         //scale and size of image
         
         bird.setScale(0.5)
-       
+        
         //Setting where the image position begins
         bird.position = CGPoint(x: self.frame.width * 0.35, y: self.frame.size.height * 0.6)
         
         
-        //Physics of the bird 
+        //Physics of the bird
         // Physics body creates a radius around the bird so it allows it to hit things
         
         
@@ -95,6 +103,25 @@ class GameScene: SKScene {
         ground.physicsBody.dynamic = false
         self.addChild(ground)
         
+        
+        
+        
+        let skyTexture = SKTexture(imageNamed: "stars")
+        skyTexture.filteringMode = .Nearest
+        
+        let moveSkySprite = SKAction.moveByX(-skyTexture.size().width * 2.0, y: 0, duration: NSTimeInterval(0.001 * skyTexture.size().width * 2.0))
+        let resetSkySprite = SKAction.moveByX(skyTexture.size().width * 2.0, y: 0, duration: 0.0)
+        let moveSkySpritesForever = SKAction.repeatActionForever(SKAction.sequence([moveSkySprite,resetSkySprite]))
+        
+        for var i:CGFloat = 0; i < 2.0 + self.frame.size.width / ( skyTexture.size().width * 2.0 ); ++i {
+            let sprite = SKSpriteNode(texture: skyTexture)
+            sprite.setScale(2.0)
+            sprite.zPosition = -20
+            sprite.position = CGPointMake(i * sprite.size.width, sprite.size.height / 2.0 + groundTexture.size().height * 2.0)
+            sprite.runAction(moveSkySpritesForever)
+           self.addChild(sprite)
+        }
+        
         //Building
         
         //Creating the pipes
@@ -104,7 +131,7 @@ class GameScene: SKScene {
         // movement of pipes
         // creates a smooth movment for the pipes
         let distanceToMove = CGFloat(self.frame.size.width + 2.0 * pipeUpTexture.size().width)
-
+        
         //grabbing the distance it needs to move and certain time interval
         let movePipes = SKAction.moveByX(-distanceToMove, y: 0.0, duration: NSTimeInterval(0.01 * distanceToMove))
         //grabbing a sequence so it moves the pipes and remove the pipes
@@ -115,10 +142,10 @@ class GameScene: SKScene {
         
         let spawn = SKAction.runBlock({() in self.spawnPipes()})
         
-            let delay = SKAction.waitForDuration(NSTimeInterval(2.0))
-            let spawnThenDelay = SKAction.sequence([spawn, delay])
-            let spawnThenDelayForever = SKAction.repeatActionForever(spawnThenDelay)
-            self.runAction(spawnThenDelayForever)
+        let delay = SKAction.waitForDuration(NSTimeInterval(2.0))
+        let spawnThenDelay = SKAction.sequence([spawn, delay])
+        let spawnThenDelayForever = SKAction.repeatActionForever(spawnThenDelay)
+        self.runAction(spawnThenDelayForever)
         
     }
     
@@ -151,7 +178,7 @@ class GameScene: SKScene {
         
         pipePair.runAction(PipesMoveAndRemove)
         self.addChild(pipePair)
-    
+        
     }
     
     override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
@@ -160,26 +187,68 @@ class GameScene: SKScene {
         for touch: AnyObject in touches {
             
             //Location of where you click  (its set to self)
-            let location = touch.locationInNode(self)
-
+      
+            
+            let touch = touches.anyObject() as UITouch
+            let touchLocation = touch.locationInNode(self)
+            
+            // Reject any shots that are below the ship, or directly to the right or left
+//            let targetingVector = touchLocation - bird.position
+            
             bird.physicsBody.velocity = CGVectorMake(0,0)
             
             bird.physicsBody.applyImpulse(CGVectorMake(0, 25))
             
-//            let sprite = SKSpriteNode(imageNamed:"Image")
-//            
-//
-//            sprite.position = location
-//            
-//            let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:5)
-//            
-//            sprite.runAction(SKAction.repeatActionForever(action))
-//        
-//            self.addChild(sprite)
+            //            let sprite = SKSpriteNode(imageNamed:"Image")
+            //
+            //
+            //            sprite.position = location
+            //
+            //            let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:5)
+            //
+            //            sprite.runAction(SKAction.repeatActionForever(action))
+            //        
+            //            self.addChild(sprite)
         }
     }
-   
+    
+    
+    
     override func update(currentTime: CFTimeInterval) {
         /* Called before each frame is rendered */
     }
+
+    
+    func fireMissile(targetingVector: CGPoint) {
+        // Now that we've confirmed that the shot is "legal", FIRE ZE MISSILES!
+        
+        // Play shooting sound
+        runAction(SKAction.playSoundFileNamed("missile.mp3", waitForCompletion: false))
+        
+        // Create the missile sprite at the ship's location
+        let missile = SKSpriteNode(imageNamed: "missile")
+        missile.position.x = bird.position.x
+        missile.position.y = bird.position.y + (bird.size.height / 2)
+        
+        // Give the missile sprite a physics body
+        missile.physicsBody = SKPhysicsBody(circleOfRadius: missile.size.width / 2)
+        missile.physicsBody.dynamic = true
+        missile.physicsBody.collisionBitMask = 0
+        missile.physicsBody.usesPreciseCollisionDetection = true
+        
+        addChild(missile)
+        
+//        // Calculate the missile's speed and final destination
+//        let direction = targetingVector.normalized
+//        let missileVector = direction * 1000
+//        let missileEndPos = missileVector + missile.position
+//        let missileSpeed: CGFloat = 500
+//        let missileMoveTime = size.width / missileSpeed
+//        
+//        // Send the missile on its way
+//        let actionMove = SKAction.moveTo(missileEndPos, duration: NSTimeInterval(missileMoveTime))
+//        let actionMoveDone = SKAction.removeFromParent()
+//        missile.runAction(SKAction.sequence([actionMove, actionMoveDone]))
+    }
+
 }
